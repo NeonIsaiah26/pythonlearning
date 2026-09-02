70d9c075f2521c10c4ee1c6401217000c04979bf
Ken <neonkenisaiah26@gmail.com>
2026-09-02 18:40:47 +0800

    new studies

--- fileread.py
+++ fileread.py
@@ -1,4 +1,4 @@
-#Reading a file and printing the contents of the file
+ # Reading a file and printing the contents of the file
 
 def fileread(file_name):
     handle = open(file_name)
@@ -6,7 +6,7 @@ def fileread(file_name):
         print(items.strip())
     handle.close()
 
-#Counting lines in a file
+ # Counting lines in a file
 
 def countlines(file_name):
     handle = open(file_name)
@@ -15,3 +15,31 @@ def countlines(file_name):
         count += 1
     print("Number of lines in the file: ", count)
     handle.close()
+ 
+ # Reading the "whole" file and printing the length of the file and the first 20 characters of the file
+
+def read_whole(file_name, print_specific):
+    handle = open(file_name)
+    inp = handle.read()
+    print(len(inp))
+    print(inp[:print_specific])
+    handle.close()
+
+# Searching through a file for a specific string and printing the lines that contain the string
+
+def search_string(file_name, search):
+    handle = open(file_name)
+    countlines(file_name)
+    for lines in handle:
+        if lines.startswith(search):
+            print(lines.strip())
+
+# Excluding lines that start with a specific string and printing the rest of the lines
+def exclude_string(file_name, exclude):
+    handle = open(file_name)
+    countlines(file_name)
+    for lines in handle:
+        if not lines.startswith(exclude):
+            print(lines.strip())
+
+exclude_string('text.txt', 'Ken')
