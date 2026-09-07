a018e510457ee47967824cdcba9a02c1144958bc
Ken <neonkenisaiah26@gmail.com>
2026-09-07 20:25:13 +0800

    new changes

--- Using Python to Access Web Data/using_urllib.py
+++ Using Python to Access Web Data/using_urllib.py
@@ -1,31 +1,40 @@
-import urllib.request, urllib.parse, urllib.error
+# import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('https://data.pr4e.org/romeo.txt')
+# fhand = urllib.request.urlopen('https://data.pr4e.org/romeo.txt')
 # for line in fhand:
 #     print(line.decode().strip())
 
 
 
-# import urllib.request
-# import ssl
+import urllib.request
+import ssl
 
-# # Create an unverified SSL context to bypass local certificate errors
-# ctx = ssl.create_default_context()
-# ctx.check_hostname = False
-# ctx.verify_mode = ssl.CERT_NONE
+# Create an unverified SSL context to bypass local certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-# # Pass the context into urlopen
-# fhand = urllib.request.urlopen('https://data.pr4e.org/romeo.txt', context=ctx)
-
-for line in fhand:
-    print(line.decode().strip())
-
+# Pass the context into urlopen
 fhand = urllib.request.urlopen('https://data.pr4e.org/romeo.txt')
-counts = dict()
 for line in fhand:
-    words = line.decode().split()
+   decoded_line = (line.decode().strip())
+   print(decoded_line)
+counts = dict()
+word = decoded_line.rsplit()
+for words in word:
+    word = line.decode().split()
     for word in words:
-        counts[word] = counts.get(word, 0) + 1
+        counts[words] = counts.get(words, 0) + 1
 print(counts)
+
+
+# for line in fhand:
+#     print(line.decode().strip())
+# counts = dict()
+# for line in fhand:
+#     words = line.decode().split()
+#     for word in words:
+#         counts[word] = counts.get(word, 0) + 1
+# print(counts)
 
 
